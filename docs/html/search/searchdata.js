8fcdca46b324716a1b23d5f68a8305fb2175b715
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-12-19 17:25:35 -0500

    Update Docs

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,24 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "dgimnoprs",
-  1: "mnps",
-  2: "mnps",
-  3: "dgimnoprs"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions"
 };
 
