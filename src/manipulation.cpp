5127208634b28d3d79d74ae6463a4140a9bc90aa
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-12-18 20:31:17 -0500

    Implement Manipulation

--- src/manipulation.cpp
+++ src/manipulation.cpp
@@ -28,24 +28,57 @@ Manipulation::Manipulation() : Node("manipulation") {
     place_pose.orientation.y = 0;
     place_pose.orientation.z = 0;
     place_pose.orientation.w = 0;
+
+    pick_client = create_client<gazebo_msgs::srv::DeleteEntity>(
+        "delete_entity");
+    place_client = create_client<gazebo_msgs::srv::SpawnEntity>(
+        "spawn_entity");
+    manip_node = rclcpp::Node::
+                    make_shared("book_manipulation_node");
 }
 
 /**
  * @brief Picks the book using the gripper.
  * 
- * @return Nothing.
+ * @return bool.
  */
 bool Manipulation::pick_book() {
-    // Check if the service is available and wait
-    return true;
+while (!pick_client->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+          RCLCPP_ERROR(this->get_logger(),
+              "Interruped while waiting for the server.");
+
+          return false;
+        }
+        RCLCPP_INFO(this->get_logger(),
+              "Server not available, waiting again...");
+    }
+    // Create a request to delete the entity
+    auto request = std::make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
+    request->name = "book1";
+
+    auto result = pick_client->async_send_request(request);
+    // Wait till the entity is deleted and the status is success
+    auto ret = rclcpp::spin_until_future_complete(manip_node,
+                                            result, 10s);
+    if (ret == rclcpp::FutureReturnCode::SUCCESS) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
  * @brief Places the book at the shelf.
  * 
- * @return Nothing.
+ * @return bool.
  */
 bool Manipulation::place_book() {
-    // Place the book at the shelf using system command
-    return true;
+    
+    auto res = system("ros2 run gazebo_ros spawn_entity.py -entity book -x 3.5 -y -2.5 -z 0 -file `ros2 pkg prefix 808x-final-project`/share/808x-final-project/models/book/model.sdf");
+    if (res > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
