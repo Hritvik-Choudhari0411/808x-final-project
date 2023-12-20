f40f2507daa9675646e18668380c29e385fc2dbb
Hritvik Choudhari <hritvikchoudhari@gmail.com>
2023-12-20 02:53:37 -0500

    Fixed perception.cpp file

--- src/perception.cpp
+++ src/perception.cpp
@@ -46,61 +46,58 @@ Perception::Perception() : Node("perception") {
  */
 void Perception::img_sensor_callback(
     const sensor_msgs::msg::Image::ConstSharedPtr& msg) {
-    try {
-        // Convert the cv_bridge image to opencv Image.
-        // cv::imshow("view", cv_bridge::toCvShare(msg, msg->encoding)->image);
-        cv::Mat image = cv_bridge::toCvShare(msg, msg->encoding)->image;
-        // HSV Masking to detect the trash bin.
-        int low_H = 40, low_S = 100, low_V = 100;
-        int high_H = 80, high_S = 255, high_V = 255;
-        cv::Mat hsv, thr, bin;
-        cv::cvtColor(image, hsv, CV_RGB2HSV);
-        // Get the HSV Mask
-        cv::inRange(hsv, cv::Scalar(low_H, low_S, low_V),
-            cv::Scalar(high_H, high_S, high_V), thr);
-        // Apply thresholding to convert to binary
-        threshold(thr, bin, 100, 255, cv::THRESH_BINARY);
-        std::vector<std::vector<cv::Point> > contours;
-        cv::Mat contourOutput = thr.clone();
-        // Find the contours in the image
-        cv::findContours(contourOutput, contours, CV_RETR_LIST,
-                    CV_CHAIN_APPROX_NONE);
-        cv::Mat contourImage(image.size(), CV_8UC3,
-                    cv::Scalar(0, 0, 0));
-        cv::Scalar colors[3];
-        colors[0] = cv::Scalar(255, 0, 0);
-        colors[1] = cv::Scalar(0, 255, 0);
-        colors[2] = cv::Scalar(0, 0, 255);
-        // Draw the contours around the detected bin
-        if (contours.size() > 0) {
-            for (size_t idx = 0; idx < contours.size(); idx++) {
-                cv::drawContours(contourImage, contours,
-                        idx, colors[idx % 3]);
-            }
-            // Create a bounding rectangle to get the center of the bin
-            cv::Rect rect;
-            rect = cv::boundingRect(contours.at(0));
-            int cent_x = static_cast<int>((rect.x+rect.width)/2);
-            int area = static_cast<int>(rect.area());
-            // Decide if the robot needs to rotate left/right/stop/move_forward
-            if (cent_x < 180) {
-                rotate_L_flag = true;
-                rotate_R_flag = false;
-            } else if (cent_x > 200) {
-                    rotate_R_flag = true;
-                    rotate_L_flag = false;
-            } else {
-                if (area > 50000) {
-                    stop_flag = true;
-                    move_forward = false;
-                } else {
-                    move_forward = true;
-                    stop_flag = false;
-                }
-                rotate_L_flag = false;
-                rotate_R_flag = false;
-            }
-            // Check the yaw to decide if the robot is rotated to detect bins
+  try {
+    // Convert the cv_bridge image to opencv Image.
+    cv::Mat image = cv_bridge::toCvShare(msg, msg->encoding)->image;
+
+    // HSV Masking to detect the trash bin.
+    int low_H = 40, low_S = 100, low_V = 100;
+    int high_H = 80, high_S = 255, high_V = 255;
+    cv::Mat hsv, thr, bin;
+    cv::cvtColor(image, hsv, cv::COLOR_RGB2HSV);
+
+    // Get the HSV Mask
+    cv::inRange(hsv, cv::Scalar(low_H, low_S, low_V),
+                cv::Scalar(high_H, high_S, high_V), thr);
+
+    // Apply thresholding to convert to binary
+    threshold(thr, bin, 100, 255, cv::THRESH_BINARY);
+
+    std::vector<std::vector<cv::Point>> contours;
+    cv::Mat contourOutput = thr.clone();
+
+    // Find the contours in the image
+    cv::findContours(contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+
+    cv::Mat contourImage(image.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Scalar colors[3];
+    colors[0] = cv::Scalar(255, 0, 0);
+    colors[1] = cv::Scalar(0, 255, 0);
+    colors[2] = cv::Scalar(0, 0, 255);
+
+    // Draw the contours around the detected bin
+    if (contours.size() > 0) {
+      for (size_t idx = 0; idx < contours.size(); idx++) {
+        cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
+      }
+
+      // Create a bounding rectangle to get the center of the bin
+      cv::Rect rect;
+      rect = cv::boundingRect(contours.at(0));
+      int cent_x = static_cast<int>((rect.x + rect.width) / 2);
+      int area = static_cast<int>(rect.area());
+
+      // Decide if the robot needs to rotate left/right/stop/move_forward
+      if (cent_x < 180) {
+        rotate_L_flag = true;
+        rotate_R_flag = false;
+      } else if (cent_x > 200) {
+        rotate_R_flag = true;
+        rotate_L_flag = false;
+      } else {
+        if (area > 50000) {
+          stop_flag = true;
+          move_forward = false;
         } else {
           move_forward = true;
           stop_flag = false;
@@ -110,27 +107,17 @@ void Perception::img_sensor_callback(
       }
       // Check the yaw to decide if the robot is rotated to detect bins
     } else {
-      if (current_yaw - init_yaw > 1.57) {
-        next_location = true;
-        rotate_L_flag = false;
-        rotate_R_flag = false;
-        move_forward = false;
-        stop_flag = true;
-      } else {
-        rotate_L_flag = true;
-        rotate_R_flag = false;
-        move_forward = false;
-        stop_flag = false;
-      }
+      move_forward = true;
+      stop_flag = false;
     }
-    // Uncomment the cv:: commented lines to visualize the output
-    // cv::imshow("view", contourImage);
-    // cv::waitKey(10);
+    rotate_L_flag = false;
+    rotate_R_flag = false;
   } catch (cv_bridge::Exception& e) {
     RCLCPP_ERROR(this->get_logger(), "Could not convert from '%s' to 'bgr8'.",
                  msg->encoding.c_str());
   }
 }
+
 
 /**
  * @brief Callback function for odom topic during the search.
