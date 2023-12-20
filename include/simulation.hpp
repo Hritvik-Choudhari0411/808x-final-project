f190948aa335c1ce3a11b0d362684d352e848845
Hritvik Choudhari <hritvikchoudhari@gmail.com>
2023-12-20 02:21:26 -0500

    Code analysis using cpplint and cppcheck

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -5,29 +5,31 @@
  * @brief RobotSim class interface
  * @version 0.1
  * @date 2023-12-10
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #pragma once
 #include <rclcpp/rclcpp.hpp>
+
 #include "./manipulation.hpp"
 #include "./navigation.hpp"
 #include "./perception.hpp"
 
 /**
- * @brief Simulation class to simulate the book collection and arranging task of the robot.
- * 
+ * @brief Simulation class to simulate the book collection and arranging task of
+ * the robot.
+ *
  */
 class Simulation {
  public:
-    /**
-    * @brief Construct a new simulation object
-    * 
-    */
-    Simulation();
+  /**
+   * @brief Construct a new simulation object
+   *
+   */
+  Simulation();
 
-    Navigation navigation;
-    Perception perception;
-    Manipulation manipulation;
+  Navigation navigation;
+  Perception perception;
+  Manipulation manipulation;
 };
--- src/manipulation.cpp
+++ src/manipulation.cpp
@@ -4,85 +4,82 @@
  * @author Hritvik Choudhari (hac@umd.edu)
  * @version 0.1
  * @date 2023-12-10
- * 
+ *
  * @copyright Copyright (c) 2023
  */
 
 #include "../include/manipulation.hpp"
+
+#include <chrono>
 #include <fstream>
 #include <ios>
 #include <rclcpp/executors.hpp>
 #include <rclcpp/future_return_code.hpp>
-#include <chrono>
 
 using namespace std::chrono_literals;
 
-
 /**
  * @brief Manipulation class constructor.
- * 
+ *
  * Initializes the Manipulation node and sets up necessary parameters.
  */
 Manipulation::Manipulation() : Node("manipulation") {
-    // Set the book place position
-    place_pose.position.x = 0;
-    place_pose.position.y = 0;
-    place_pose.position.z = 0;
-    place_pose.orientation.x = 0;
-    place_pose.orientation.y = 0;
-    place_pose.orientation.z = 0;
-    place_pose.orientation.w = 0;
+  // Set the book place position
+  place_pose.position.x = 0;
+  place_pose.position.y = 0;
+  place_pose.position.z = 0;
+  place_pose.orientation.x = 0;
+  place_pose.orientation.y = 0;
+  place_pose.orientation.z = 0;
+  place_pose.orientation.w = 0;
 
-    pick_client = create_client<gazebo_msgs::srv::DeleteEntity>(
-        "delete_entity");
-    place_client = create_client<gazebo_msgs::srv::SpawnEntity>(
-        "spawn_entity");
-    manpltn_node = rclcpp::Node::
-                    make_shared("book_manipulation_node");
+  pick_client = create_client<gazebo_msgs::srv::DeleteEntity>("delete_entity");
+  place_client = create_client<gazebo_msgs::srv::SpawnEntity>("spawn_entity");
+  manpltn_node = rclcpp::Node::make_shared("book_manipulation_node");
 }
 
 /**
  * @brief Picks the book using the gripper.
- * 
+ *
  * @return bool.
  */
 bool Manipulation::pick_book() {
-while (!pick_client->wait_for_service(1s)) {
-        if (!rclcpp::ok()) {
-          RCLCPP_ERROR(this->get_logger(),
-              "Interruped while waiting for the server.");
+  while (!pick_client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(),
+                   "Interruped while waiting for the server.");
 
-          return false;
-        }
-        RCLCPP_INFO(this->get_logger(),
-              "Server not available, waiting again...");
+      return false;
     }
-    // Create a request to delete the entity
-    auto request = std::make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
-    request->name = "book1";
+    RCLCPP_INFO(this->get_logger(), "Server not available, waiting again...");
+  }
+  // Create a request to delete the entity
+  auto request = std::make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
+  request->name = "book1";
 
-    auto result = pick_client->async_send_request(request);
-    // Wait till the entity is deleted and the status is success
-    auto ret = rclcpp::spin_until_future_complete(manpltn_node,
-                                            result, 10s);
-    if (ret == rclcpp::FutureReturnCode::SUCCESS) {
-        return true;
-    } else {
-        return false;
-    }
+  auto result = pick_client->async_send_request(request);
+  // Wait till the entity is deleted and the status is success
+  auto ret = rclcpp::spin_until_future_complete(manpltn_node, result, 10s);
+  if (ret == rclcpp::FutureReturnCode::SUCCESS) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
  * @brief Places the book at the shelf.
- * 
+ *
  * @return bool.
  */
 bool Manipulation::place_book() {
-    
-    auto res = system("ros2 run gazebo_ros spawn_entity.py -entity book -x 3.5 -y -2.5 -z 0 -file `ros2 pkg prefix 808x-final-project`/share/808x-final-project/models/book/model.sdf");
-    if (res > 0) {
-        return true;
-    } else {
-        return false;
-    }
+  auto res = system(
+      "ros2 run gazebo_ros spawn_entity.py -entity book -x 3.5 -y -2.5 -z 0 "
+      "-file `ros2 pkg prefix "
+      "808x-final-project`/share/808x-final-project/models/book/model.sdf");
+  if (res > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
